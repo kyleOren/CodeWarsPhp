b2332187a11916781014a06f958e73778e79ece1
Kyle Gibson <kyle.gibson@smarterbusiness.co.uk>
2019-07-11 16:52:11 +0200

    added a new challeneg

--- Count of positives sum of negatives/Solution.php
+++ Count of positives sum of negatives/Solution.php
@@ -1,28 +1,44 @@
-<?php
-$input =array(1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15);
-countPositivesSumNegatives($input);
-    function countPositivesSumNegatives($input) {
-        sort($input);
-        $counter;
-        foreach($input as $key=>$value){
-            if($value > 0){
-                $positive[] = $value;
-                $key ++;
+function countPositivesSumNegatives($input) {
+  $resultarr = array();
 
-            }else{
-                $negative[] = $value;
-            }
-        }
-        $counter = $key;
-        $temp;
-        $sum;
-        foreach($negative as $derp=>$value){
-           $derp ++;
-           $sum = $sum + $value ;
-        }
-        $finalCounter = $counter - $derp;
-        $resultarr = array();
-        array_push($resultarr,$finalCounter,$sum);
-    return $resultarr;
+  if(empty($input)){
+
+  }else{
+    sort($input);
+    $counter = 0;
+    foreach($input as $key=>$value){
+      if($value > 0){
+        $derp[] = $value;
+      }else if($value < 0){
+        $derp[] = $value;
+      }else{
+
+      }
+    }
+    foreach($derp as $key=>$value){
+      if($value > 0){
+        $positive[] = $value;
+        $key ++;
+      }else if($value == 0){
+        $key = $key - 1;
+      }else{
+        $negative[] = $value;
+      }
+    }
+    if($key > 0){
+    $counter = $key;
+    }else{
+
+    }
+    $temp = 0;
+    $sum = 0;
+    foreach($negative as $derp=>$value){
+      $derp ++;
+      $sum = $sum + $value;
+    }
+    $finalCounter = $counter - $derp;
+    array_push($resultarr,$finalCounter,$sum);
+    print_r(" first $resultarr[0] second $resultarr[1]");
+  }
+  return $resultarr;
 }
-?>
