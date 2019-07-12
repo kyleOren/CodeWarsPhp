b8949c9af42789773e76ad484e6a53cbf5d33a24
Kyle Gibson <kyle.gibson@smarterbusiness.co.uk>
2019-07-12 13:07:17 +0200

    added new challenge and updated count challenge

--- Total amount of points/Solution.php
+++ Total amount of points/Solution.php
@@ -1,3 +1,20 @@
 <?php
-
- ?>
+  $games = array("1:1","2:0","3:0","1:2");
+  points($games);
+  function points(array $games): int {
+    $points = 0;
+    foreach ($games as $key => $value) {
+      $x = substr($value,0,1);
+      $y = substr($value,2,3);
+      if($x > $y){
+        $points = $points + 3;
+      }else if($x < $y){
+        $points = $points + 0 ;
+      }else if($x == $y){
+        $points = $points + 1;
+      }
+    }
+    print_r("score = $points");
+    return 0;
+  }
+?>
